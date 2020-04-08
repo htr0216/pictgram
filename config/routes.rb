0412cfb416ecb2583f22212feaacb5b3b50d43e6
kobayashihotaru <blue02.sh@icloud.com>
2020-04-08 02:43:40 +0000

    ログインフォーム画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'pages/index'
+  root 'pages#index'
+  get 'pages/help'
+  
+  resources :users
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "pages#index"
-  get "pages/help"
+  
+  
 end
