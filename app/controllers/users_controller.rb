75f30e869c30b0d4c13cac77be805c3fd2ef4def
kobayashihotaru <blue02.sh@icloud.com>
2020-04-08 05:44:13 +0000

    Userの新規登録機能のベース作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,12 @@ class UsersController < ApplicationController
   end
   
   def create
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+  
 end
