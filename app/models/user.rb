66c942c67c242000360ad05056d0ea700ba947b9
kobayashihotaru <blue02.sh@icloud.com>
2020-04-11 08:26:30 +0000

    モデルの関連付け(アソシエーション)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
   validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
   
-  
   has_secure_password
+   
+  has_many :topics
 end
 
